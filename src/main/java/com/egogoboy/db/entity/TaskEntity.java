380f00df228371c834a8f3a5ef58467b5a6e9210
egogoboy <egor2005sudakov@gmail.com>
2026-02-27 13:00:48 +0300

    entity: add TaskEntity

---
+++ src/main/java/com/egogoboy/db/entity/TaskEntity.java
@@ -0,0 +1,37 @@
+package com.egogoboy.db.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class TaskEntity {
+    @Id
+    @GeneratedValue
+    @Column
+    Long id;
+
+    @Column
+    String title;
+
+    @Column
+    String description;
+
+    @Column(name = "start_time")
+    Timestamp startTimestamp;
+
+    @Column(name = "end_time")
+    Timestamp endTimestamp;
+}
