d4059806d58f150996cef6a85d3b59f878353e3b
egogoboy <egor2005sudakov@gmail.com>
2026-02-27 13:01:08 +0300

    UserEntity: add lombok annotations

--- src/main/java/com/egogoboy/db/entity/UserEntity.java
+++ src/main/java/com/egogoboy/db/entity/UserEntity.java
@@ -5,7 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 class UserEntity {
     @Id
     @GeneratedValue
