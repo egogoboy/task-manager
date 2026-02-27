b1fb6a4d0ba794d94cfb62ae27a521f1af3ed3dc
egogoboy <egor2005sudakov@gmail.com>
2026-02-27 13:16:03 +0300

    entity: add UsersTasksEntity

---
+++ src/main/java/com/egogoboy/db/entity/UsersTasksEntity.java
@@ -0,0 +1,29 @@
+package com.egogoboy.db.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users_tasks")
+class UsersTasksEntity {
+
+    @Column
+    @JoinColumn(name = "user_id", nullable = false)
+    UserEntity user;
+
+    @Column
+    @JoinColumn(name = "task_id", nullable = false)
+    TaskEntity task;
+
+}
