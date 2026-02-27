f516ed2a85c2a700cb4ab2cbfa6f9e0aa636bbc3
egogoboy <egor2005sudakov@gmail.com>
2026-02-27 16:40:23 +0300

    repository: add TaskAssignmentRepository

---
+++ src/main/java/com/egogoboy/repository/TaskAssignmentRepository.java
@@ -0,0 +1,83 @@
+package com.egogoboy.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import com.egogoboy.db.entity.TaskAssignmentEntity;
+
+@ApplicationScoped
+class TaskAssignmentRepository {
+    EntityManager entityManager;
+
+    @Inject
+    TaskAssignmentRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    TaskAssignmentEntity findById(Long id) {
+        if (id == null) {
+            return null;
+        }
+
+        TaskAssignmentEntity taskAssignment = entityManager.find(TaskAssignmentEntity.class, id);
+
+        return taskAssignment;
+    }
+
+    List<TaskAssignmentEntity> findByUserId(Long user_id) {
+        if (user_id == null) {
+            return null;
+        }
+
+        TypedQuery<TaskAssignmentEntity> query = entityManager.createQuery(
+                "SELECT e FROM TaskAssignmentEntity e WHERE e.user_id == :user_id",
+                TaskAssignmentEntity.class);
+
+        return query.getResultList();
+    }
+
+    List<TaskAssignmentEntity> findByTaskId(Long task_id) {
+        if (task_id == null) {
+            return null;
+        }
+
+        TypedQuery<TaskAssignmentEntity> query = entityManager.createQuery(
+                "SELECT e FROM TaskAssignmentEntity e WHERE e.task_id == :task_id",
+                TaskAssignmentEntity.class);
+
+        return query.getResultList();
+    }
+
+    List<TaskAssignmentEntity> findAll() {
+        TypedQuery<TaskAssignmentEntity> query = entityManager.createQuery(
+                "SELECT e FROM TaskAssignmentEntity e",
+                TaskAssignmentEntity.class);
+
+        return query.getResultList();
+    }
+
+    @Transactional
+    void save(TaskAssignmentEntity taskAssignment) {
+        if (taskAssignment.getId() == null) {
+            entityManager.persist(taskAssignment);
+        } else {
+            entityManager.merge(taskAssignment);
+        }
+
+        entityManager.flush();
+    }
+
+    @Transactional
+    void deleteById(Long id) {
+        TaskAssignmentEntity entity = findById(id);
+        if (entity != null) {
+            entityManager.remove(entity);
+        }
+    }
+}
